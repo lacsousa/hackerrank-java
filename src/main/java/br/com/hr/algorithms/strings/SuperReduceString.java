ea1488a5e809e91da6c440371cda09121f6e320f
Luciano Cordeiro <lacsousa@gmail.com>
2021-07-25 22:27:26 -0300

    Início dos exercícios de Strings

---
+++ src/main/java/br/com/hr/algorithms/strings/SuperReduceString.java
@@ -0,0 +1,39 @@
+package br.com.hr.algorithms.strings;
+
+import java.util.Scanner;
+
+public class SuperReduceString {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // String s = "aabc";
+        String s = scan.next();
+
+        // String s1 = "aaabccddd";
+        // String s2 = "abba"; 
+        if (s.length() < 1 || s.length() > 100) {
+            System.exit(0);
+        }
+        scan.close();
+        reduzString(s);
+        // reduzString(s1);
+        // reduzString(s2);
+    }
+
+    private static void reduzString(String str) {
+       
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                str = str.substring(0, i - 1) + str.substring(i + 1);
+                i = 0;
+            }
+        }
+        if (str.length() == 0) {
+            System.out.println("Empty String");
+        } else {
+            System.out.println(str);
+        }
+    }
+}
